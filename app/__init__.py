e157b8e7736a3bfd965161292fedcf1df13ffabc
Sagiruddin Mondal <sagir.sagir@gmail.com>
2020-11-01 11:52:48 +0530

    initial scaffolding setup

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask_restplus import Api
+from flask import Blueprint
+
+from .main.controllers.user_controller import api as user_ns
+
+blueprint = Blueprint('api', __name__)
+
+api = Api(blueprint,
+          title='FLASK RESTPLUS API BOILER-PLATE ',
+          version='1.0',
+          description='simple flask web services'
+          )
+api.add_namespace(user_ns, path='/user')
